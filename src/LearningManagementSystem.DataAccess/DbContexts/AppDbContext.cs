9080b192529e3ac3eb54e4bae81ea46ca3626751
Erqoziyev <erqoziyevshodiyor@gmail.com>
2024-02-02 10:59:14 +0500

    Added DbContext

---
+++ src/LearningManagementSystem.DataAccess/DbContexts/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LearningManagementSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningManagementSystem.DataAccess.DbContexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Lesson> Lessons { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Subject> Subjects { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Attendance> Attendances { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+}
---
+++ src/LearningManagementSystem.WebApi/Configurations/LayerConfigurations/DataAccessConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using LearningManagementSystem.DataAccess.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningManagementSystem.WebApi.Configurations.LayerConfigurations;
+
+public static class DataAccessConfiguration
+{
+    public static void ConfigureDataAccess(this WebApplicationBuilder builder)
+    {
+        string connectionString = builder.Configuration.GetConnectionString("DatabaseConnectionString")!;
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+    }
+}
